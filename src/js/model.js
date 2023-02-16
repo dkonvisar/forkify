3ba0d4ca8c6ea57f87ad61fe823b1c7b61c7d720
denis <denis.konvisar@gmail.com>
2023-02-16 16:47:08 +0200

    implemented search results

--- src/js/model.js
+++ src/js/model.js
@@ -3,11 +3,15 @@ import { getJSON } from './helpers';
 
 export const state = {
   recipe: {},
+  search: {
+    query: '',
+    results: [],
+  },
 };
 
 export const loadRecipe = async function (id) {
   try {
-    const data = await getJSON(`${API_URL}/${id}`);
+    const data = await getJSON(`${API_URL}${id}`);
 
     const { recipe } = data.data;
 
@@ -29,4 +33,23 @@ export const loadRecipe = async function (id) {
   }
 };
 
-console.log(state);
+export const loadSearchResults = async function (query) {
+  try {
+    state.search.query = query;
+
+    const data = await getJSON(`${API_URL}?search=${query}`);
+    console.log(data);
+
+    state.search.results = data.data.recipes.map(rec => {
+      return {
+        id: rec.id,
+        title: rec.title,
+        publisher: rec.publisher,
+        image: rec.image_url,
+      };
+    });
+  } catch {
+    console.error(`${err} 🧨🧨🧨`);
+    throw err;
+  }
+};
