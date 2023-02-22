f322c15bd1d9603ea552b63b696a7c9898d4bc4a
denis <denis.konvisar@gmail.com>
2023-02-22 23:56:17 +0200

    some cleaned up

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = 'afc8e89b-99d8-4485-adb1-98eccaa7d9e2';
+export const KEY = '8c1e7820-8c29-4b58-82df-f06194923db9';
 export const MODAL_CLOSE_SEC = 2.5;
